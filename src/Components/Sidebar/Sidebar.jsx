976fc9414c4b825eecc45339499b07c26761b784
salvi <salvilopezronda@gmail.com>
2021-05-24 18:56:28 +0200

    ddd

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -130,7 +130,8 @@ const useStyles = makeStyles (theme => ({
     //Container del Dashboard
     container:{
         paddingBottom: theme.spacing(4),
-        paddingTop: theme.spacing(10)
+        paddingTop: theme.spacing(10),
+        paddingLeft: theme.spacing(10)
     }
 }))
 
